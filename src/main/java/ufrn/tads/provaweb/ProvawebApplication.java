25ff4ec387fd156f7e32e2d86254189f19fb17ac
Ana Camilly <camillyguedes2@gmail.com>
2022-07-01 16:43:24 -0300

    iniciando o projeto

---
+++ src/main/java/ufrn/tads/provaweb/ProvawebApplication.java
@@ -0,0 +1,13 @@
+package ufrn.tads.provaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvawebApplication.class, args);
+	}
+
+}
